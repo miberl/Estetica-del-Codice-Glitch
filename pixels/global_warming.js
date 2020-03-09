4dc4ddcb13a514549972849ce834628082ee54a2
Michael B <47005505+michaelberli@users.noreply.github.com>
2020-03-09 18:07:32 +0100

    Add files via upload

--- pixels/global_warming.js
+++ pixels/global_warming.js
@@ -25,30 +25,60 @@ function draw() {
 
     switch (phase) {
         case 0:
-
-            new Glitch(img).show();
-            if (floor(random(0,3))==0)
-                new ColouredBars(img,floor(random(0,8))).showSingleBar();
-            if (iterate == 30) {
+            image(img, 0, 0, dim, dim);
+            iterate++;
+            if (iterate == 10) {
                 phase = 1;
                 iterate = 0;
             }
-            console.log("Hi");
-            iterate++;
             break;
 
         case 1:
+            new Glitch(img).show();
+            if (floor(random(0, 3)) == 0) {
+                for (let i = 0; i < floor(random(1, 4)); i++)
+                    new ColouredBars(img, floor(random(0, 8))).showSingleBar();
+            }
+
+            iterate++;
+            if (iterate == 30) {
+                phase = 2;
+                iterate = 0;
+            }
+            break;
+
+        case 2:
+            console.log("Hello");
+            new ColouredBars(img, iterate).show();
+
+            iterate++;
+            if (iterate == 10) {
+                iterate = 0;
+                phase = 3;
+            }
+            break;
+
+        case 3:
+            new ColouredBars(img, iterate).antiShow();
+            iterate++;
+            if (iterate == 8) {
+                iterate = 4;
+                phase = 4;
+            }
+            break;
+
+        case 4:
             console.log(iterate);
             new Pixelate(img, iterate).pixels();
             iterate = iterate * 2;
             if (iterate == 1024) {
-                phase = 2;
+                phase = 5;
                 iterate = 0;
             }
             tick.play();
             break;
 
-        case 2:
+        case 5:
 
             new Dark(img).allDark();
             if (iterate == 0) {
@@ -58,12 +88,12 @@ function draw() {
                 console.log("Star initialized");
             }
             if (iterate == 2)
-                phase = 3;
+                phase = 6;
             iterate++;
             tick.play();
             break;
 
-        case 3:
+        case 6:
 
             for (let i = 0; i < emissions.length; i++) {
                 stelle[i].show();
@@ -71,14 +101,14 @@ function draw() {
             console.log("Star viewed");
 
             if (iterate == 5) {
-                phase = 4;
+                phase = 7;
             }
             tick.play();
             iterate++;
             break;
 
 
-        case 4:
+        case 7:
 
             tint(iterate, 0, 0);
             image(img, 0, 0, dim, dim);
@@ -87,17 +117,11 @@ function draw() {
             }
             iterate += 15;
             if (iterate > 255)
-                phase = 5;
+                phase = 8;
             tick.play();
             break;
 
-        case 6:
-            console.log("Hello");
-            new ColouredBars(img,iterate).show();
-            if (iterate==8)
-                iterate=0;
-            iterate++;
-            break;
+
 
         default:
             console.log("Execution finished");
